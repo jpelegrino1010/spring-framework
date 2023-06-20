a87582f07ef8682edd93d8ea2ada2c9c2d152a12
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-06-19 20:10:25 -0400

    bean validation

--- easyschool/src/main/java/com/julioluis/easyschool/model/Contact.java
+++ easyschool/src/main/java/com/julioluis/easyschool/model/Contact.java
@@ -1,12 +1,19 @@
 package com.julioluis.easyschool.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class Contact {
 
+    @NotBlank(message = "Name must no be blank")
     private String name;
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNum;
+    @Email(message = "Provide a valid email address")
+    @NotBlank(message = "email must no be blank")
     private String email;
     private String subject;
     private String message;
