11d602b01ff4cae2e6bf6379af16ddbf84d6f0de
jpelegrino1010 <julio.pelegrino@viseo.com>
2023-08-02 18:15:04 -0400

    pagination

--- easyschool/src/main/java/com/julioluis/easyschool/repository/ContactRepository.java
+++ easyschool/src/main/java/com/julioluis/easyschool/repository/ContactRepository.java
@@ -2,7 +2,13 @@ package com.julioluis.easyschool.repository;
 
 import com.julioluis.easyschool.model.Contact;
 import com.julioluis.easyschool.rommappers.ContactRowMapper;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
@@ -15,9 +21,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface ContactRepository extends CrudRepository<Contact,Integer> {
+public interface ContactRepository extends JpaRepository<Contact,Integer> {
 
 List<Contact> findContactByStatus(String status);
+Page<Contact> findContactByStatus(String status, Pageable pageable);
 
-
+@Transactional
+@Modifying
+@Query(value = "UPDATE Contact c set c.status=?1 where c.id=?2")
+int updateStatus(String status,Integer contactId);
 }
